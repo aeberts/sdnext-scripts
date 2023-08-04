8bc8b3d2b90f3ad03db5dcdce068663ef29fc256
Alexander Eberts <alex.eberts@gmail.com>
2023-08-04 17:20:37 -0400

    Create install_deps.sh

---
+++ install_deps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt-get install vim unzip libgl1-mesa-dev libcairo2-dev wget git -y
+apt-get install python3 python3-venv python3-dev build-essential -y
+adduser user1 --disabled-password --gecos ""
